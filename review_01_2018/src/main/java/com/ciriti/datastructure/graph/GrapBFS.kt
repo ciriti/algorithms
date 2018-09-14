f7aaa24c4d486ec4bc49d3894fbdadcbe9cf9b2d
ciriti <ciriti@gmail.com>
2018-09-14 08:57:02 +0200

    graph

---
+++ review_01_2018/src/main/java/com/ciriti/datastructure/graph/GrapBFS.kt
@@ -0,0 +1,13 @@
+package com.ciriti.datastructure.graph
+
+import java.util.Queue
+
+/**
+ * Created by ciriti
+ */
+
+fun Node<Char>.bfs() : Int{
+
+
+  return 0
+}
--- review_01_2018/src/main/java/com/ciriti/datastructure/graph/GraphElement.kt
+++ review_01_2018/src/main/java/com/ciriti/datastructure/graph/GraphElement.kt
@@ -3,19 +3,50 @@ package com.ciriti.datastructure.graph
 /**
  * Created by ciriti
  */
-class Node<T>(
+
+data class Node<T>(
   val value: T,
-  val args: () -> List<Node<T>> = emptyList<>()
+  val args: List<Node<T>> = emptyList()
 )
 
-object NB {
-  operator fun <T> get(vararg a: T) = listOf(*a)
+object N {
+  operator fun get(
+    b: Int,
+    vararg a: Node<Int>
+  ) = Node(b, listOf(*a))
 }
 
-object N {
-  operator fun <T> get(m: Int) = Node(m)
+object C {
+  operator fun get(
+    b: Char,
+    vararg a: Node<Char>
+  ) = Node(b, listOf(*a))
 }
+
+fun <T> Node<T>.print() = println("[ val: $value, neighbour size: ${args.size} ]")
+
+fun <T> Node<T>.printChild() {
+  print(" $value ")
+  if (args.isNotEmpty()) args.forEach {
+    print(", ")
+    it.printChild()
+  }
+}
+
 
 fun main(args: Array<String>) {
+  val n =
+    N[1,
+        N[2,
+            N[22],
+            N[23],
+            N[24],
+            N[25]
+        ],
+        N[3],
+        N[4],
+        N[5]]
+
+  n.printChild()
 
 }
