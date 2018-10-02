350ae5e6c6691135c43919338769fa6ea4baeed2
ciriti <ciriti@gmail.com>
2018-10-02 19:57:56 +0200

    stack complete

--- review_01_2018/src/main/java/com/ciriti/datastructure/Stack.kt
+++ review_01_2018/src/main/java/com/ciriti/datastructure/Stack.kt
@@ -5,46 +5,64 @@ package com.ciriti.datastructure
  */
 class Stack<T> : Collection<T> {
 
-    private var head: Node<T>? = null
+  private var head: Node<T>? = null
 
-    override var size: Int = 0
-        private set
+  override var size: Int = 0
+    private set
 
-    override fun contains(element: T): Boolean {
-        return true
+  override fun contains(element: T): Boolean {
+    for (obj in this) {
+      if (obj == element) return true
     }
+    return false
+  }
 
-    override fun containsAll(elements: Collection<T>): Boolean {
-        return true
+  override fun containsAll(elements: Collection<T>): Boolean {
+    return true
+  }
+
+  override fun isEmpty(): Boolean {
+    return size == 0
+  }
+
+  override fun iterator(): Iterator<T> {
+    return object : Iterator<T> {
+
+      var node: Node<T>? = head
+
+      override fun hasNext(): Boolean = node != null
+
+      override fun next(): T {
+        if (!hasNext()) throw NoSuchElementException()
+        val current = node!!
+        node = current.next
+        return current.value
+      }
     }
+  }
 
-    override fun isEmpty(): Boolean {
-        return size == 0
-    }
+  fun peek(): T? {
+    return head?.value
+  }
 
-    override fun iterator(): Iterator<T> {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+  fun push(elem: T) {
+    size++
+    val node = Node(elem)
+    node.next = head
+    head = node
+  }
 
-    fun peek() : T?{
-        return head?.value
-    }
+  fun pop(): T? {
+    if (size == 0) return null
+    size--
+    val res = head!!.value
+    val newHead = head!!.next
+    head = newHead
+    return res
+  }
 
-    fun push(elem: T) {
-        size++
-        val node = Node(elem)
-        node.next = head
-        head = node
-    }
-
-    fun pop(): T? {
-        if(size == 0) return null
-        size--
-        val res = head!!.value
-        head!!.next = head!!.next?.next
-        head = null
-        return res
-    }
-
-    private class Node<T>(val value: T, var next: Node<T>? = null)
+  private class Node<T>(
+    val value: T,
+    var next: Node<T>? = null
+  )
 }
