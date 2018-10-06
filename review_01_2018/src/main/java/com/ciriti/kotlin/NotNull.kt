697d58cfa79c70f85ecf18ce0b3b2a3276e939f6
ciriti <ciriti@gmail.com>
2018-10-06 18:49:23 +0200

    lambda and higher order function

--- review_01_2018/src/main/java/com/ciriti/kotlin/NotNull.kt
+++ review_01_2018/src/main/java/com/ciriti/kotlin/NotNull.kt
@@ -3,11 +3,17 @@ package com.ciriti.kotlin
 /**
  * Created by Carmelo Iriti
  */
-fun main(args: Array<String>) {
-  val map = mutableMapOf<Int, Int>()
-//  map[2].executeIfNull { map[2] = 2 }
-//  map[2] = 2
-  map[2]?.let {
-    println("I'm $it")
-  }
+//fun main(args: Array<String>) {
+//  val map = mutableMapOf<Int, Int>()
+//  //map[1] ?: map[1] = 1 // it doesn't compile
+//  //map[1].runIfNull { map[1] = 1 } // it compiles
+//  with(map[1]){
+//    println(this)
+//  }
+//}
+//
+//fun <T> T.runIfNull(func: () -> Unit) = this ?: func()
+
+fun <T, R> myWith(receiver: T, block: T.() -> R): R {
+  return receiver.block()
 }
