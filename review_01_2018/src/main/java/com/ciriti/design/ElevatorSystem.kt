11384d46219d3f73ce07556aa853dda295842f91
ciriti <ciriti@gmail.com>
2018-10-10 20:23:45 +0200

    elevator design

--- review_01_2018/src/main/java/com/ciriti/design/ElevatorSystem.kt
+++ review_01_2018/src/main/java/com/ciriti/design/ElevatorSystem.kt
@@ -1,39 +1,117 @@
 package com.ciriti.design
 
+import com.ciriti.design.Floor.THIRD
+import java.lang.RuntimeException
+import java.security.InvalidParameterException
+
 /**
  * Created by Carmelo Iriti
  */
 
-fun main(args : Array<String>){
-    var state = Floor_.GROUND
-    while(state.next() != Floor_.NOTHING){
-        println("$state -> ${state.next()}")
-        println("${state.ordinal} -> ${state.next().ordinal}")
-        state = state.next()
-    }
-    while(state.prev() != Floor_.NOTHING){
-        println("$state -> ${state.prev()}")
-        println("${state.ordinal} -> ${state.prev().ordinal}")
-        state = state.prev()
-    }
+fun main(args: Array<String>) {
+  val elevator = ElevatorController(THIRD)
+  val view = View()
+  elevator.onfloorListener {
+    println(it)
+  }
+  view.onClickListener {
+    elevator.selectFloor(0)
+    elevator.selectFloor(1)
+    elevator.selectFloor(3)
+  }
+  view.click()
 }
 
-class  Elevator{
-
-    var floorState : Floor_ = Floor_.GROUND
-    var floorSelected : MutableSet<Floor_> = mutableSetOf()
-
-    var floorListener : ((floor : Floor_)-> Unit)? = null
-    var stateListener : ((floor : Floor_)-> Unit)? = null
-
-    fun selectFloor(nextFloor : Int){
-
-    }
-
+fun main() {
+  var state = Floor.GROUND
+  while (state.next() != Floor.NOTHING) {
+    println("$state -> ${state.next()}")
+    println("${state.ordinal} -> ${state.next().ordinal}")
+    state = state.next()
+  }
+  while (state.prev() != Floor.NOTHING) {
+    println("$state -> ${state.prev()}")
+    println("${state.ordinal} -> ${state.prev().ordinal}")
+    state = state.prev()
+  }
 }
 
+class View {
+  private var clickListener: ((view: View) -> Unit)? = null
+  fun onClickListener(func: (view: View) -> Unit) {
+    clickListener = func
+  }
 
+  fun click() {
+    clickListener?.invoke(this)
+  }
+}
 
+interface IElevator {
+  fun selectFloor(floors: Int)
+}
+
+class ElevatorController(var floorState: Floor = Floor.GROUND) : IElevator {
+
+  private var floorListener: ((floor: Floor) -> Unit)? = null
+
+  fun onfloorListener(func: (floor: Floor) -> Unit) {
+    floorListener = func
+  }
+
+  override fun selectFloor(floor: Int) {
+    floor.checkRange(0, Floor.THIRD.ordinal)
+    while (floorState.ordinal != floor) {
+      when {
+        floorState.ordinal < floor -> {
+          floorState = floorState.next()
+          floorListener?.invoke(floorState)
+        }
+        else -> {
+          floorState = floorState.prev()
+          floorListener?.invoke(floorState)
+        }
+      }
+    }
+  }
+}
+
+fun Int.checkRange(
+  min: Int,
+  max: Int,
+  exception: RuntimeException = InvalidParameterException("Floor not valid")
+) = when {
+  this < min -> throw exception
+  this > max -> throw exception
+  else -> this
+}
+
+enum class Floor {
+
+  GROUND {
+    override fun prev() = NOTHING
+    override fun next() = FIRST
+  },
+  FIRST {
+    override fun prev() = GROUND
+    override fun next() = SECOND
+  },
+  SECOND {
+    override fun prev() = FIRST
+    override fun next() = THIRD
+  },
+  THIRD {
+    override fun prev() = SECOND
+    override fun next() = NOTHING
+  },
+  NOTHING {
+    override fun prev() = THIRD
+    override fun next() = GROUND
+  };
+
+  abstract fun prev(): Floor
+  abstract fun next(): Floor
+}
 
 //sealed class Floor(val floor : Int){
 //    abstract fun prev() : Floor
@@ -55,29 +133,3 @@ class  Elevator{
 //    override fun prev(): Floor = Ground()
 //    override fun next(): Floor = Ground()
 //}
-
-enum class Floor_{
-
-    GROUND{
-        override fun prev() = NOTHING
-        override fun next() = FIRST
-    },
-    FIRST{
-        override fun prev() = GROUND
-        override fun next() = SECOND
-    },
-    SECOND{
-        override fun prev() = FIRST
-        override fun next() = THIRD
-    },
-    THIRD{
-        override fun prev() = SECOND
-        override fun next() = NOTHING
-    },NOTHING{
-        override fun prev() = THIRD
-        override fun next() = GROUND
-    };
-
-    abstract fun prev() : Floor_
-    abstract fun next() : Floor_
-}
