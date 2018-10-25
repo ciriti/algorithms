b1960c483dddfcdd8f7a920dc8569f1b92538749
ciriti <ciriti@gmail.com>
2018-10-25 09:20:18 +0200

    intent service

---
+++ review_01_2018/src/main/java/com/ciriti/kotlin/MyIntentService.kt
@@ -0,0 +1,16 @@
+package com.ciriti.kotlin
+
+import android.app.IntentService
+import android.content.Intent
+
+/**
+ * Created by Carmelo Iriti
+ */
+
+class MyIntentService : IntentService("test"){
+  override fun onHandleIntent(intent: Intent?) {
+    TODO(
+        "not implemented"
+    ) //To change body of created functions use File | Settings | File Templates.
+  }
+}
